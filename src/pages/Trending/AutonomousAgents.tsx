f8cad9d88cb0902ad846f38ed0df4bbb76cdceb3
thuongdo <thuong.dn@orai.io>
2024-12-18 16:52:04 +0700

    update UI

--- src/pages/Trending/AutonomousAgents.tsx
+++ src/pages/Trending/AutonomousAgents.tsx
@@ -1,3 +1,4 @@
+import { maxBTCAvatar, spotlightBg } from "@assets/images"
 import { IconTrendingUp } from "@components/Icons/DefiLens"
 import { MessagePlusIcon } from "@components/Icons/Message"
 import { TelegramOutlineIcon } from "@components/Icons/SocialLinkIcon"
@@ -6,7 +7,7 @@ import { TwitterIcon } from "@components/Icons/Twitter"
 const AutonomousAgents: React.FC = () => {
   return (
     <div className="flex w-full items-center gap-10">
-      <div className="w-[60%]">
+      <div className="w-[55%]">
         <div className="flex items-center gap-4">
           <h1 className="text-[68px] font-bold">8</h1>
           <span className="text-[22px] font-bold text-mercury-950">
@@ -37,27 +38,41 @@ const AutonomousAgents: React.FC = () => {
         </div>
       </div>
 
-      <div className="min-h-[200px] w-[40%] rounded-[22px] bg-[#FF075A] p-[18px]">
-        <div className="flex h-full min-h-[200px] flex-col justify-between">
-          <div>
-            <h3 className="text-[32px] font-[800] uppercase text-white">max</h3>
-            <div className="w-fit rounded-[4px] border border-[rgba(255,255,255,0.40)] px-2">
+      <div className="relative min-h-[280px] w-[45%]">
+        <div
+          style={{
+            backgroundImage: `url(${spotlightBg})`,
+          }}
+          className="absolute top-1/2 h-[185px] min-w-[400px] -translate-y-1/2 rounded-[22px] bg-cover bg-center bg-no-repeat p-[18px]"
+        >
+          <div className="flex h-full flex-col justify-between">
+            <div>
+              <h3 className="text-[32px] font-[800] uppercase text-white">
+                max
+              </h3>
+              <div className="w-fit rounded-[4px] border border-[rgba(255,255,255,0.40)] px-2">
+                <span className="text-[13px] font-medium uppercase text-white">
+                  spotlight
+                </span>
+              </div>
+            </div>
+
+            <div>
               <span className="text-[13px] font-medium uppercase text-white">
-                spotlight
+                $35.1 MKT Cap
               </span>
+              <div className="mt-2 flex items-center gap-3">
+                <TwitterIcon color="#FFFF" />
+                <TwitterIcon color="#FFFF" />
+                <TelegramOutlineIcon color="#FFFF" />
+              </div>
             </div>
           </div>
 
-          <div>
-            <span className="text-[13px] font-medium uppercase text-white">
-              $35.1 MKT Cap
-            </span>
-            <div className="mt-2 flex items-center gap-3">
-              <TwitterIcon color="#FFFF" />
-              <TwitterIcon color="#FFFF" />
-              <TelegramOutlineIcon color="#FFFF" />
-            </div>
-          </div>
+          <img
+            src={maxBTCAvatar}
+            className="absolute -top-[28px] right-[50px] h-[239px] w-[180px]"
+          />
         </div>
       </div>
     </div>
