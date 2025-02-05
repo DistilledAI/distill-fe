70e32d6aa2a682266609b748ae22363768bdf51c
haitran <haitu.dev@gmail.com>
2025-02-05 17:54:41 +0700

    update display vault default

--- src/pages/Stake/SimpleStaking/index.tsx
+++ src/pages/Stake/SimpleStaking/index.tsx
@@ -51,61 +51,6 @@ const SimpleStaking = () => {
                   <StakeTable list={list} getListUnbonding={getListUnbonding} />
                 </>
               )}
-              {isGuardToken && (
-                <div className="mt-6">
-                  <div className="flex items-center gap-2 text-[22px] font-semibold max-md:text-[18px]">
-                    <GiftIcon color="#A2845E" size={24} /> What Are The
-                    Incentives for Staking?
-                  </div>
-                  <div className="mt-4">
-                    <p className="text-18 font-semibold text-[#2CB34E] max-md:text-15">
-                      Profit Distribution
-                    </p>
-                    <p className="font-medium text-mercury-800 max-md:text-14">
-                      Rewards come from the stakers’ reward pool, funded by
-                      profits from the DCA fund after expiration.
-                    </p>
-                  </div>
-                  <div className="mt-3">
-                    <p className="text-18 font-semibold text-[#2CB34E] max-md:text-15">
-                      Token Contributions
-                    </p>
-                    <p className="font-medium text-mercury-800 max-md:text-14">
-                      A portion of staked $GUARD tokens from the staking vault
-                      also contributes to the reward pool.
-                    </p>
-                  </div>
-                  <div className="mt-3">
-                    <p className="text-18 font-semibold text-[#2CB34E] max-md:text-15">
-                      Revenue Streams
-                    </p>
-                    <p className="font-medium text-mercury-800 max-md:text-14">
-                      Additional funding comes from revenue-generating products.
-                    </p>
-                  </div>
-                  <div className="mt-6 flex items-center gap-2 text-[22px] font-semibold max-md:text-[18px]">
-                    <TimeIcon /> How Are Rewards Distributed?
-                  </div>
-                  <p className="mt-1 font-medium text-mercury-800 max-md:text-14">
-                    Vault $GUARD stakers receive rewards based on their staked
-                    weight{" "}
-                    <span className="font-semibold text-mercury-950">
-                      after the DCA fund expires
-                    </span>
-                    .
-                  </p>
-                  <div className="mt-6 flex items-center gap-2 text-[22px] font-semibold max-md:text-[18px] max-md:leading-5">
-                    <ChartIcon /> Can I withdraw my staked $GUARD anytime?
-                  </div>
-                  <p className="mt-1 font-medium text-mercury-800 max-md:text-14">
-                    No, staked $GUARD in the vault cannot be claimed to{" "}
-                    <span className="font-semibold text-mercury-950">
-                      ensure long-term sustainability
-                    </span>
-                    .
-                  </p>
-                </div>
-              )}
             </>
           ) : (
             <div className="flex h-[200px] flex-col items-center justify-center rounded-[22px] border-1 border-dashed border-mercury-400 bg-mercury-70">
@@ -115,6 +60,61 @@ const SimpleStaking = () => {
               <p className="mt-2 font-medium text-mercury-950">No staking</p>
               <p className="text-14 text-mercury-700">
                 Your staking status will appear here.
+              </p>
+            </div>
+          )}
+          {isGuardToken && (
+            <div className="mt-6">
+              <div className="flex items-center gap-2 text-[22px] font-semibold max-md:text-[18px]">
+                <GiftIcon color="#A2845E" size={24} /> What Are The Incentives
+                for Staking?
+              </div>
+              <div className="mt-4">
+                <p className="text-18 font-semibold text-[#2CB34E] max-md:text-15">
+                  Profit Distribution
+                </p>
+                <p className="font-medium text-mercury-800 max-md:text-14">
+                  Rewards come from the stakers’ reward pool, funded by profits
+                  from the DCA fund after expiration.
+                </p>
+              </div>
+              <div className="mt-3">
+                <p className="text-18 font-semibold text-[#2CB34E] max-md:text-15">
+                  Token Contributions
+                </p>
+                <p className="font-medium text-mercury-800 max-md:text-14">
+                  A portion of staked $GUARD tokens from the staking vault also
+                  contributes to the reward pool.
+                </p>
+              </div>
+              <div className="mt-3">
+                <p className="text-18 font-semibold text-[#2CB34E] max-md:text-15">
+                  Revenue Streams
+                </p>
+                <p className="font-medium text-mercury-800 max-md:text-14">
+                  Additional funding comes from revenue-generating products.
+                </p>
+              </div>
+              <div className="mt-6 flex items-center gap-2 text-[22px] font-semibold max-md:text-[18px]">
+                <TimeIcon /> How Are Rewards Distributed?
+              </div>
+              <p className="mt-1 font-medium text-mercury-800 max-md:text-14">
+                Vault $GUARD stakers receive rewards based on their staked
+                weight{" "}
+                <span className="font-semibold text-mercury-950">
+                  after the DCA fund expires
+                </span>
+                .
+              </p>
+              <div className="mt-6 flex items-center gap-2 text-[22px] font-semibold max-md:text-[18px] max-md:leading-5">
+                <ChartIcon /> Can I withdraw my staked $GUARD anytime?
+              </div>
+              <p className="mt-1 font-medium text-mercury-800 max-md:text-14">
+                No, staked $GUARD in the vault cannot be claimed to{" "}
+                <span className="font-semibold text-mercury-950">
+                  ensure long-term sustainability
+                </span>
+                .
               </p>
             </div>
           )}
