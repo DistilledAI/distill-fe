4549c3e060248c64dc46899f9cc699ac15c54fca
huynhtd <huynhtran924@gmail.com>
2025-02-20 19:00:40 +0700

    Done add virtualizer, load more agent clans store

--- src/reducers/agentSlice.ts
+++ src/reducers/agentSlice.ts
@@ -16,6 +16,7 @@ export interface IMyAgent {
   avatar: string | null
   typeAgent: AGENT_TYPE_KEY
   contractAddress?: string | null
+  typeAgent: AGENT_TYPE_KEY
 }
 
 export interface AgentState {
