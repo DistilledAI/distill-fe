4ce312fc3fdddea301720c71731bc766b0c5472a
haitran <haitu.dev@gmail.com>
2025-02-24 15:36:35 +0700

    knowledge

---
+++ src/components/AgentSetupStatus/index.tsx
@@ -0,0 +1,51 @@
+import { ChevronDownIcon } from "@components/Icons/ChevronDownIcon"
+import { DatabaseIcon } from "@components/Icons/DatabaseImportIcon"
+import { FilledShieldCheckedIcon } from "@components/Icons/FilledShieldCheck"
+import { borderGdImg } from "@assets/images"
+import { FilledBrainAIIcon } from "@components/Icons/BrainAIIcon"
+
+const AgentSetupStatus = () => {
+  return (
+    <div className="relative mx-auto mb-4 flex max-w-[684px] items-center justify-between md:mb-6">
+      {/* Agent Status */}
+      <div className="flex flex-1 flex-col items-center gap-1">
+        <div className="relative flex h-8 w-8 items-center justify-center rounded-full bg-[#FC0] md:h-10 md:w-10">
+          <FilledBrainAIIcon color="#363636" size={24} />
+        </div>
+        <div className="flex flex-col items-center">
+          <span className="text-14 font-semibold text-mercury-950 md:text-18">
+            Your Agent will be created
+          </span>
+          <span className="text-14 font-semibold text-mercury-950 md:text-18">
+            with Private Intelligence
+          </span>
+        </div>
+      </div>
+
+      {/* Chevron Divider */}
+      <div className="absolute left-1/2 top-4 w-full max-w-[150px] -translate-x-1/2 md:max-w-[280px]">
+        <img src={borderGdImg} alt="border gradient" />
+        <div className="absolute -top-[9px] left-1/2">
+          <div className="rotate-90">
+            <ChevronDownIcon color="#9D936E" size={22} />
+          </div>
+        </div>
+      </div>
+
+      {/* Database Connection Status */}
+      <div className="flex flex-1 flex-col items-center gap-1">
+        <div className="relative flex h-8 w-8 items-center justify-center rounded-full bg-mercury-200 md:h-10 md:w-10">
+          <DatabaseIcon color="#363636" size={24} />
+          <div className="absolute -bottom-1 -right-1">
+            <FilledShieldCheckedIcon color="#A2845E" />
+          </div>
+        </div>
+        <p className="max-w-[227px] text-center text-14 font-semibold text-mercury-950 max-md:text-center md:text-18">
+          Add anything you want the agent to learn
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default AgentSetupStatus
--- src/pages/CreateAgent/Content/Knowledge/index.tsx
+++ src/pages/CreateAgent/Content/Knowledge/index.tsx
@@ -1,5 +1,18 @@
+import AgentSetupStatus from "@components/AgentSetupStatus"
+import { DatabaseImportIcon } from "@components/Icons/DatabaseImportIcon"
+
 const Knowledge = () => {
-  return <div>Knowledge</div>
+  return (
+    <div>
+      <div className="flex items-center gap-1">
+        <DatabaseImportIcon />
+        <span className="text-[22px] font-semibold">Connected Sources</span>
+      </div>
+      <div className="mt-5">
+        <AgentSetupStatus />
+      </div>
+    </div>
+  )
 }
 
 export default Knowledge
