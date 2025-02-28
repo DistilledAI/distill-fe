3a10311e144a0cfb2a5cd7dd55dcb0b0bbc9b3ce
huynhtd <huynhtran924@gmail.com>
2025-02-28 17:16:40 +0700

    Done refactor chat window v2

--- src/pages/AgentClans/index.tsx
+++ src/pages/AgentClans/index.tsx
@@ -5,8 +5,8 @@ import AllClans from "./OtherAgentClans/AllClans"
 const AgentClans = () => {
   return (
     <div className="flex h-full">
-      <aside className="w-[250px] border-r border-mercury-200">
-        <div className="fixed -mt-[68px] h-full w-[250px] px-3 pt-2">
+      <aside className="w-[250px]">
+        <div className="fixed -mt-[68px] h-full w-[250px] border-r border-r-mercury-200 px-3 pt-2">
           <h2 className="text-32 font-bold text-mercury-950">Clans</h2>
           <div className="mt-4 space-y-3">
             <h3 className="text-14 font-medium text-mercury-800">
