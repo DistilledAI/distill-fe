4e7bcaa1fbcc66f0a48c333114db660f762c656b
haitran <haitu.dev@gmail.com>
2025-03-11 14:39:12 +0700

    update social agent data

--- src/pages/AgentDetail/AgentContent/Knowledge/AddData/Social/helpers.ts
+++ src/pages/AgentDetail/AgentContent/Knowledge/AddData/Social/helpers.ts
@@ -1,9 +1,27 @@
-export const getUserName = (url: string) => {
-  if (!url) return null
+export const getUserName = (input: string) => {
+  if (!input) return null
+
+  let url: string
+  try {
+    const parsedInput = JSON.parse(input)
+    if (parsedInput?.url?.value) {
+      url = parsedInput.url.value
+    } else {
+      return null
+    }
+  } catch (e) {
+    url = input
+  }
 
   const match = url.match(/x\.com\/([^/]+)/)
   if (match && match[1] !== "home") {
     return match[1]
   }
   return null
+}
+
+export const getLinkXByURL = (input: string) => {
+  const username = getUserName(input)
+  if (username) return `https://x.com/${username}`
+  return `https://x.com/${input}`
 }
