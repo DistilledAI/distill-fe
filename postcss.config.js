3d7b31791a1ae771ecf39f6595a6b84384b4025e
huynhtd <huynhtran924@gmail.com>
2025-03-18 11:42:24 +0700

    Done remove css nano

--- postcss.config.js
+++ postcss.config.js
@@ -1,21 +1,6 @@
-const isProduction =
-  process.env.VITE_BASE_API_URL === "https://prod-api.distilled.ai/distill"
-
 export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
-    ...(isProduction && {
-      cssnano: {
-        preset: [
-          "default",
-          {
-            discardComments: { removeAll: true },
-            normalizeWhitespace: true,
-            mergeRules: false,
-          },
-        ],
-      },
-    }),
   },
 }
