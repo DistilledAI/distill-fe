e1abac46ce3a7361f4a9c8b3b1141bf8c3470e2c
huynhtd <huynhtran924@gmail.com>
2025-03-24 11:55:06 +0700

    Update: enhance ProposalsDetail component with navigation

--- src/pages/Dao/ProposalsDetail/index.tsx
+++ src/pages/Dao/ProposalsDetail/index.tsx
@@ -5,16 +5,28 @@ import useProposalDetail from "./useProposalDetail"
 import useProposalIpfs from "../Proposals/useProposalIpfs"
 import { Skeleton } from "@nextui-org/react"
 import { twMerge } from "tailwind-merge"
+import { useLocation, useNavigate } from "react-router-dom"
 
 const ProposalsDetail = () => {
+  const navigate = useNavigate()
+  const location = useLocation()
+
   const { proposalDetail, getProposalsDetail } = useProposalDetail()
   const { proposalIpfs } = useProposalIpfs({
     uri: proposalDetail?.uri,
   })
 
+  const handleBack = () => {
+    const daoPath = location.pathname.split("/proposals/")[0]
+    navigate(`${daoPath}/proposals`)
+  }
+
   return (
     <div className="mx-auto min-h-dvh max-w-[1024px] p-4 pb-20 pt-14 md:p-6 md:pt-0">
-      <BackButton className="fixed left-0 top-0 h-[50px] pl-4 max-md:h-[40px] max-md:w-full max-md:bg-white" />
+      <BackButton
+        className="fixed left-0 top-0 h-[50px] pl-4 max-md:h-[40px] max-md:w-full max-md:bg-white"
+        onClick={handleBack}
+      />
       <div className="grid grid-cols-1 gap-5 md:grid-cols-[350px_1fr] md:gap-10">
         <Skeleton
           isLoaded={!!proposalDetail}
