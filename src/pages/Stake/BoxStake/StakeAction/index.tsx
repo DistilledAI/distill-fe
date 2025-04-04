901e5b21ab1b597196b37159b47aca30f91c1b1e
haitran <haitu.dev@gmail.com>
2025-04-04 23:54:43 +0700

    off deposit banker vault

--- src/pages/Stake/BoxStake/StakeAction/index.tsx
+++ src/pages/Stake/BoxStake/StakeAction/index.tsx
@@ -32,6 +32,9 @@ const StakeAction: React.FC<{
   const { balance, loading, getBalance } = useGetBalance(tokenAddress)
   const tokenInfo = getInfoTokenByAddress(tokenAddress as StakeTokenAddress)
   const isExpired = endDate ? Date.now() > endDate : false
+  const isOffStake = [StakeTokenAddress.Banker].includes(
+    tokenAddress as StakeTokenAddress,
+  )
 
   const web3Locking = new Web3SolanaLockingToken()
 
@@ -64,7 +67,7 @@ const StakeAction: React.FC<{
 
   const handleStake = async () => {
     try {
-      if (isExpired) return
+      if (isExpired || isOffStake) return
       if (!tokenAddress) {
         return toast.warning("Token address not found!")
       }
@@ -106,74 +109,78 @@ const StakeAction: React.FC<{
 
   return (
     <div className="mt-3">
-      <div className="rounded-lg border-1 border-mercury-400 bg-white px-4 py-3">
-        <div className="flex items-center justify-between gap-1">
-          <div>
-            <NumberFormat
-              placeholder={`0.0`}
-              thousandSeparator
-              className="w-full bg-transparent text-[24px] font-medium capitalize text-mercury-950 outline-none [appearance:textfield] placeholder:text-[#585A6B] [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none"
-              decimalScale={SPL_DECIMAL}
-              type="text"
-              value={amountVal}
-              onChange={() => {}}
-              isAllowed={(values) => {
-                const { floatValue } = values
-                return !floatValue || (floatValue >= 0 && floatValue <= 1e14)
-              }}
-              onValueChange={({ floatValue }: any) => {
-                handleInputChange(floatValue)
-              }}
-            />
+      <div className={`${isOffStake ? "pointer-events-none opacity-50" : ""}`}>
+        <div className="rounded-lg border-1 border-mercury-400 bg-white px-4 py-3">
+          <div className="flex items-center justify-between gap-1">
+            <div>
+              <NumberFormat
+                placeholder={`0.0`}
+                thousandSeparator
+                className="w-full bg-transparent text-[24px] font-medium capitalize text-mercury-950 outline-none [appearance:textfield] placeholder:text-[#585A6B] [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none"
+                decimalScale={SPL_DECIMAL}
+                type="text"
+                value={amountVal}
+                onChange={() => {}}
+                isAllowed={(values) => {
+                  const { floatValue } = values
+                  return !floatValue || (floatValue >= 0 && floatValue <= 1e14)
+                }}
+                onValueChange={({ floatValue }: any) => {
+                  handleInputChange(floatValue)
+                }}
+              />
 
-            <div className="flex items-center gap-1 text-14 font-medium text-mercury-700 max-md:text-12">
-              <p>Available:</p>
-              <p>
-                {loading
-                  ? "--"
-                  : numberWithCommas(toBN(balance.toFixed(3)).toNumber())}{" "}
-                {tokenInfo?.tokenName}
-              </p>
+              <div className="flex items-center gap-1 text-14 font-medium text-mercury-700 max-md:text-12">
+                <p>Available:</p>
+                <p>
+                  {loading
+                    ? "--"
+                    : numberWithCommas(
+                        toBN(balance.toFixed(3)).toNumber(),
+                      )}{" "}
+                  {tokenInfo?.tokenName}
+                </p>
+              </div>
+            </div>
+            <div>
+              <SelectToken />
             </div>
           </div>
-          <div>
-            <SelectToken />
-          </div>
         </div>
-      </div>
-      <div className="mt-2 flex items-center gap-2">
-        {AMOUNT_LIST.map((percent) => (
-          <div
-            onClick={() => setAmountVal(percent.value.toString())}
-            key={percent.label}
-            className="cursor-pointer rounded-[4px] border-1 border-mercury-300 bg-mercury-100 px-2 py-1 text-12 font-medium text-mercury-900 hover:opacity-80"
+        <div className="mt-2 flex items-center gap-2">
+          {AMOUNT_LIST.map((percent) => (
+            <div
+              onClick={() => setAmountVal(percent.value.toString())}
+              key={percent.label}
+              className="cursor-pointer rounded-[4px] border-1 border-mercury-300 bg-mercury-100 px-2 py-1 text-12 font-medium text-mercury-900 hover:opacity-80"
+            >
+              {percent.label}
+            </div>
+          ))}
+        </div>
+        {endDate && (
+          <div className="mt-4 text-14 font-medium text-brown-500">
+            Expiration Date (UTC): {moment(endDate).utc().format("lll")}
+          </div>
+        )}
+        {isConnectWallet ? (
+          <Button
+            isDisabled={isExpired || isOffStake}
+            isLoading={loadingSubmit}
+            onClick={handleStake}
+            className="mt-7 h-[48px] w-full rounded-full bg-mercury-950 text-white"
           >
-            {percent.label}
-          </div>
-        ))}
+            Deposit
+          </Button>
+        ) : (
+          <Button
+            onPress={() => setVisible(true)}
+            className="mt-7 h-[48px] w-full rounded-full bg-mercury-950 text-white"
+          >
+            CONNECT WALLET
+          </Button>
+        )}
       </div>
-      {endDate && (
-        <div className="mt-4 text-14 font-medium text-brown-500">
-          Expiration Date (UTC): {moment(endDate).utc().format("lll")}
-        </div>
-      )}
-      {isConnectWallet ? (
-        <Button
-          isDisabled={isExpired}
-          isLoading={loadingSubmit}
-          onClick={handleStake}
-          className="mt-7 h-[48px] w-full rounded-full bg-mercury-950 text-white"
-        >
-          Deposit
-        </Button>
-      ) : (
-        <Button
-          onPress={() => setVisible(true)}
-          className="mt-7 h-[48px] w-full rounded-full bg-mercury-950 text-white"
-        >
-          CONNECT WALLET
-        </Button>
-      )}
     </div>
   )
 }
